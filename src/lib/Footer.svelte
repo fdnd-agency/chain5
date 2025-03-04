3e7c97d29970e40994065f10b51ec34e1080c18b
Justus Sturkenboom <justus@ju5tu5.nl>
2025-03-04 11:25:30 +0100

    Basic layout

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,15 @@
+<footer>
+   <p><span>&copy;</span> Copyleft 2025, all wrongs reversed.</p>
+</footer>
+
+
+<style>
+  footer {
+    padding: 3rem;
+  }
+  span {
+    display: inline-block;
+    transform-origin: 50%;
+    transform: rotate(180deg);
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+   import {Footer} from '$lib'
+	let {children} = $props();
+</script>
+
+<header></header>
+
+<main>
+  {@render children()}
+</main>
+
+<Footer />
+
+<style>
+  main {
+    padding: 3rem;
+  }
+</style>
